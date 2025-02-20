ab36a07b13be6611ad021dd2f4c18ec2702d9771
Claudio Roberto <107327025+claudiorobertodev@users.noreply.github.com>
2025-02-20 16:31:59 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// Exemplo de um script simples para efeito de scroll suave
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
